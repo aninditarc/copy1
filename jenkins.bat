829f191903b666c572a6cae79155d2023616aa38
aninditarc <31618255+aninditarc@users.noreply.github.com>
2017-09-04 17:57:08 +0530

    Add files via upload

---
+++ jenkins.bat
@@ -0,0 +1,8 @@
+@echo off
+Rem This dumpscript
+@echo off
+mkdir C:\apps\workspace\DevOps
+echo.>"C:\apps\workspace\DevOps\file.txt"
+mkdir C:\apps\workspace\Dump
+copy C:\apps\workspace\DevOps\file.txt C:\apps\workspace\Dump
+ 
