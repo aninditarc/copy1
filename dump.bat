560523679fdcd0d9ea8d25e0ceab7551de09d599
aninditarc <31618255+aninditarc@users.noreply.github.com>
2017-09-05 16:57:21 +0530

    Delete dump.bat

--- dump.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-Rem This dumpscript
-@echo off
-mkdir C:\apps\workspace\DevOps
-echo.>"C:\apps\workspace\DevOps\file.txt"
-mkdir C:\apps\workspace\Dump
-copy C:\apps\workspace\DevOps\file.txt C:\apps\workspace\Dump
- 
