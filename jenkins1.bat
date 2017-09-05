60feed36f40c867ebbc464a6670230c22deffecb
aninditarc <31618255+aninditarc@users.noreply.github.com>
2017-09-05 16:55:45 +0530

    Add files via upload

---
+++ jenkins1.bat
@@ -0,0 +1,10 @@
+ @echo off
+Rem This practise sessions
+mkdir C:\apps\workspace\DEV
+dir>"C:\apps\workspace\DEV\copy.txt"
+Rem This a devops folder created and a text file which is presently stores directory data.
+mkdir C:\apps\workspace\DEV1
+Rem This it is a dumb devops folder 
+copy C:\apps\workspace\DEV\copy.txt C:\apps\workspace\DEV1
+Rem This Dumpscript 
+ 
